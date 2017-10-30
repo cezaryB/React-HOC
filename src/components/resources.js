aed3d746e2e3118dfad12b0e6677dbcb0c9d29e4
Cezary B <czarekburzykowski@gmail.com>
2017-10-30 22:11:47 +0100

    setting up Redux

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      Ugachaka
+      <ul>
+        <li>1 dice</li>
+        <li>2 dices</li>
+        <li>Penny and a dime</li>
+      </ul>
+    </div>
+  )
+}
